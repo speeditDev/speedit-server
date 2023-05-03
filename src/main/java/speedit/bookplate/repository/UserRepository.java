55994075d42b8b6c40a4842107e8e7d1e5858009
eunyoung23 <eun970923@naver.com>
2023-05-03 16:38:26 +0900

    Refactor : 불필요한 코드 수정

--- src/main/java/speedit/bookplate/repository/UserRepository.java
+++ src/main/java/speedit/bookplate/repository/UserRepository.java
@@ -3,7 +3,6 @@ package speedit.bookplate.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import speedit.bookplate.domain.User;
-import speedit.bookplate.utils.enumTypes.JobCategory;
 
 import java.util.List;
 import java.util.Optional;
